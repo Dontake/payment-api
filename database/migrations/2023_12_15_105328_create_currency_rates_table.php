b22cf63900903096e49b8de09ad526c0db0b9218
Vlad Kiryakov(Dondake) <wlad.kiryakov@gmail.com>
2023-12-15 15:34:00 +0400

    ver-1-0: !create migrations

---
+++ database/migrations/2023_12_15_105328_create_currency_rates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currency_rates', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('currency_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('exchange_id')->constrained(
+                table: 'currencies', indexName: 'currency_rates_exchange_id'
+            )->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->integer('current_rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currency_rates');
+    }
+};
